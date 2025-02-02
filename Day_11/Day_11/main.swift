c687a54c2d303b771c3a3e032daa6fd9cf27ae17
JPR <jan@pruner.dev>
2025-02-02 12:42:02 +0100

    Day 11 - Task 2 - works, but slow

--- Day_11/Day_11/main.swift
+++ Day_11/Day_11/main.swift
@@ -1,41 +1,90 @@
 import Foundation
 import System
-/*
-func ProcessStone(_ index: Int) -> Int {
-    if(index >= numberLine.count) { return 0 }
-    
-    let val = numberLine[index]
-    
-    // 0 -> 1
-    if val == 0{
-        numberLine[index] = 1
-    }
-    // even number of digits -> split into text halfs
-    else if (String(val).count % 2 == 0) {
-        let text = String(val)
-        let indexOfHalfLeft = text.index(text.startIndex, offsetBy: (Int)(text.count-1)/2)
-        let indexOfHalfRight = text.index(text.startIndex, offsetBy: text.count/2)
-        
-        let leftHalf: String = String(text[...indexOfHalfLeft])
-        let rightHalf: String = String(text[indexOfHalfRight...])
-        numberLine[index] = Int(leftHalf)!
-        let secondIndex = index + 1
-        numberLine.insert(Int(rightHalf)!, at: secondIndex)
-        return ProcessStone(index+2)
-    }
-    // anything else -> *2024
-    else {
-        numberLine[index] = 2024 * val
-    }
-    
-    return ProcessStone(index+1)
-}
-*/
 
 let inputFileURL = FileManager.default.homeDirectoryForCurrentUser.appending(
     path: "source/AdventOfCode2024/Day_11/AoC2024_input.11")
 
 var numberLine: LinkedList = LinkedList()
+
+let MaxIterationCount: Int = 75
+let MaxChunkSize: Int = 5000000
+
+func Iterate(_ iterationStep: Int, _ list: LinkedList) -> Int {
+    var sum : Int = 0
+    
+    let count = list.count()
+    print("\(iterationStep) : \(count)")
+    if(iterationStep == MaxIterationCount) {
+        return count
+    }
+    
+    if(count > MaxChunkSize) {
+        // split off first chunk and process it
+        var node : LinkedNode = list.head!
+        var i:Int = 1
+        while i < MaxChunkSize { node = node.next!; i += 1; }
+        
+        var startNode : LinkedNode? = list.head
+        list.head = node.next
+        node.next = nil
+        
+        let splittedList: LinkedList = LinkedList()
+        splittedList.head = startNode
+        
+        while startNode != nil {
+            startNode = ProcessNode(startNode!)
+        }
+        
+        sum += Iterate(iterationStep + 1, splittedList)
+    }
+    
+    var node : LinkedNode? = list.head
+    
+    while node != nil {
+        node = ProcessNode(node!)
+    }
+    
+    return sum + Iterate(iterationStep + 1, list)
+}
+
+func ProcessNode(_ currentNode: LinkedNode)  -> LinkedNode? {
+    // 0 -> 1
+    if currentNode.value == 0{
+        currentNode.value = 1
+    }
+    // even number of digits -> split into text halfs
+    else if (String(currentNode.value).count % 2 == 0) {
+        let text = String(currentNode.value)
+        let indexOfHalfLeft = text.index(text.startIndex, offsetBy: (Int)(text.count-1)/2)
+        let indexOfHalfRight = text.index(text.startIndex, offsetBy: text.count/2)
+        
+        let leftHalf: String = String(text[...indexOfHalfLeft])
+        let rightHalf: String = String(text[indexOfHalfRight...])
+        
+        // replace the current node value
+        currentNode.value = Int(leftHalf)!
+        
+        // inject the next node with rightHalf value
+        let newNode = LinkedNode(value: Int(rightHalf)!)
+        if(currentNode.next == nil) {
+            currentNode.next = newNode
+            return newNode.next
+        } else {
+            let nextNode = currentNode.next
+            
+            newNode.next = nextNode
+            currentNode.next = newNode
+            
+            return newNode.next
+        }
+    }
+    // anything else -> *2024
+    else {
+        currentNode.value = 2024 * currentNode.value
+    }
+    // move to the next node
+    return currentNode.next
+}
 
 // MAIN
 let input: String = try String(contentsOf: inputFileURL, encoding: .ascii).trimmingCharacters(in: .whitespacesAndNewlines)
@@ -44,56 +93,7 @@ input.split(separator: " ").forEach { numberStr in
     numberLine.append(Int(numberStr)!)
 }
 
-for iterationStep in 0..<75 {
-    //_ = ProcessStone(0)
-    // iterate one by one
 
-    var node : LinkedNode? = numberLine.head
-    
-    var index: Int = 0
-    while node != nil {
-        
-        // 0 -> 1
-        if node!.value == 0{
-            node!.value = 1
-        }
-        // even number of digits -> split into text halfs
-        else if (String(node!.value).count % 2 == 0) {
-            let text = String(node!.value)
-            let indexOfHalfLeft = text.index(text.startIndex, offsetBy: (Int)(text.count-1)/2)
-            let indexOfHalfRight = text.index(text.startIndex, offsetBy: text.count/2)
-            
-            let leftHalf: String = String(text[...indexOfHalfLeft])
-            let rightHalf: String = String(text[indexOfHalfRight...])
-            
-            // replace the current node value
-            node!.value = Int(leftHalf)!
-            
-            // inject the next node with rightHalf value
-            let newNode = LinkedNode(value: Int(rightHalf)!)
-            if(node!.next == nil) {
-                node!.next = newNode
-                node = newNode
-            } else {
-                let nextNode = node!.next
-                
-                newNode.next = nextNode
-                node!.next = newNode
-                
-                node = newNode
-            }
-            index += 1
-        }
-        // anything else -> *2024
-        else {
-            node!.value = 2024 * node!.value
-        }
-        // move to the next node
-        node = node!.next
-        index += 1
-    }
-    print("\(iterationStep) : \(index)")
-}
-
-// over
-print(numberLine.count())
+let sum = Iterate(0, numberLine)
+print(sum)
+// over  for 25 it is 233050
