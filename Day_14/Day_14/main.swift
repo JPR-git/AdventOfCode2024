b944fccc20073524465478022959244c7fdfaac5
JPR <jan@pruner.dev>
2025-02-18 12:39:35 +0100

    Day 14 - Task 1 - 211773366

--- Day_14/Day_14/main.swift
+++ Day_14/Day_14/main.swift
@@ -2,9 +2,9 @@ import Foundation
 import System
 
 let inputFileURL = FileManager.default.homeDirectoryForCurrentUser.appending(
-    path: "source/AdventOfCode2024/Day_14/AoC2024_sample.14")
-let wide: Int = 11 //101
-let tall: Int = 7 //103
+    path: "source/AdventOfCode2024/Day_14/AoC2024_input.14")
+let wide: Int = 101 //11
+let tall: Int = 103 //7
 
 // p=9,5 v=-3,-3
 let parsedLine = try Regex("^p=([\\-0-9]+),([\\-0-9]+) v=([\\-0-9]+),([\\-0-9]+)$", as: (Substring, Substring, Substring, Substring, Substring).self)
@@ -20,6 +20,27 @@ for try await line in inputFileURL.lines {
 }
 print("Robots: \(robots.count)")
 
-for i in 0..<100 {
-    
+for _ in 0..<100 {
+    for robot in robots {
+        robot.move(wide, tall)
+    }
 }
+
+var quadrant1: Int = 0
+var quadrant2: Int = 0
+var quadrant3: Int = 0
+var quadrant4: Int = 0
+
+for robot in robots {
+    switch robot.quadrant(wide, tall){
+        case 1: quadrant1 += 1
+        case 2: quadrant2 += 1
+        case 3: quadrant3 += 1
+        case 4: quadrant4 += 1
+        default: break
+    }
+}
+
+print("\(quadrant1)\t\(quadrant2)\n\(quadrant3)\t\(quadrant4)\n")
+
+print("Safety: \(quadrant1*quadrant2*quadrant3*quadrant4)")
--- Day_14/Day_14/robot.swift
+++ Day_14/Day_14/robot.swift
@@ -13,4 +13,51 @@ class Robot {
         self.Position = position
         self.Velocity = velocity
     }
+    
+    func move(_ wide: Int, _ tall: Int) {
+        self.Position.0 += self.Velocity.0
+        self.Position.1 += self.Velocity.1
+        
+        // EVALUATE
+        // top
+        if self.Position.1 < 0 {
+            self.Position.1 = tall + self.Position.1
+        }
+        // bottom
+        if self.Position.1 >= tall {
+            self.Position.1 = self.Position.1 - tall
+        }
+        // left
+        if self.Position.0 < 0 {
+            self.Position.0 = wide + self.Position.0
+        }
+        // right
+        if self.Position.0 >= wide {
+            self.Position.0 = self.Position.0 - wide
+        }
+    }
+    
+    func quadrant(_ wide: Int, _ tall: Int) -> Int {
+        if self.Position.0 < Int(wide/2) &&
+            self.Position.1 < Int(tall/2) {
+            return 1
+        }
+        
+        if self.Position.0 > Int(wide/2) &&
+            self.Position.1 < Int(tall/2) {
+            return 2
+        }
+        
+        if self.Position.0 < Int(wide/2) &&
+            self.Position.1 > Int(tall/2) {
+            return 3
+        }
+        
+        if self.Position.0 > Int(wide/2) &&
+            self.Position.1 > Int(tall/2) {
+            return 4
+        }
+        
+        return 0
+    }
 }
