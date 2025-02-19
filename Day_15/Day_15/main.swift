e2df1247e820bfdb0c526bfb94ac1cbcad7e3c15
JPR <jan@pruner.dev>
2025-02-19 13:41:57 +0100

    Day 15 - Task 1 - 1318523

--- Day_15/Day_15/main.swift
+++ Day_15/Day_15/main.swift
@@ -2,7 +2,48 @@ import Foundation
 import System
 
 let inputFileURL = FileManager.default.homeDirectoryForCurrentUser.appending(
-    path: "source/AdventOfCode2024/Day_15/AoC2024_sample.15")
+    path: "source/AdventOfCode2024/Day_15/AoC2024_input.15")
 
 // MAIN
+var map: [[Character]] = []
+var commands: [Character] = []
+for try await line in inputFileURL.lines {
+    let lineArray = Array(line)
+    if( lineArray[0] == "#") {
+        map.append(lineArray)
+    } else {
+        commands = commands + lineArray
+    }
+}
 
+var warehouse: Matrix = Matrix(data: map)
+print("Warehouse: \(warehouse.rows)x\(warehouse.columns)")
+
+guard var robot = warehouse.locateFirst("@") else {
+    print("No robot found!")
+    exit(1)
+}
+print("Robot: (\(robot.0):\(robot.1)) - \(Matrix.ConvertToGPS(robot))")
+
+for movement in commands {
+    guard let newRobotCoordinates = warehouse.move(robot, movement) else {
+        //print("No movement [\(robot.0),\(robot.1)]")
+        continue
+    }
+    //print("\(movement) [\(newRobotCoordinates.0),\(newRobotCoordinates.1)]")
+    robot = newRobotCoordinates
+    //print(warehouse.ToString())
+}
+
+print(warehouse.ToString())
+guard let boxes = warehouse.locate("O") else {
+    print("no boxes found.")
+    exit(2)
+}
+
+var sum: Int = 0
+for box in boxes {
+    sum += Matrix.ConvertToGPS(box)
+}
+print("Sum: \(sum)")
+print("Game over!")
